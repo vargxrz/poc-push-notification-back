f7ec499a9c9f63b7d064242311d54a91b8453c54
João Vargas <joao.vargas@NOTESEG-DEV06.segalas.local>
2024-07-03 17:29:47 -0300

    teste

--- fcm-backend/src/main/java/com/example/fcm_backend/WebConfig.java
+++ fcm-backend/src/main/java/com/example/fcm_backend/WebConfig.java
@@ -6,17 +6,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig  {
+public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("https://poc-push-notification-tawny.vercel.app/**")
-                        .allowedMethods("*")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*");
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowedOrigins("https://poc-push-notification-tawny.vercel.app")
+                        .allowedHeaders("Authorization", "Content-Type", "X-Requested-With")
+                        .allowCredentials(true);
             }
         };
     }
